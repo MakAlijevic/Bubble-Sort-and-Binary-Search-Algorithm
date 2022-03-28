5078e7220f5f7a6b6489e34a6d3f81a079791681
MakAlijevic <alijevic.mak@gmail.com>
2022-03-28 13:02:27 +0200

    Implemented file writer to print sorted IP addresses into new CSV file.

--- src/main/java/ibu/edu/App.java
+++ src/main/java/ibu/edu/App.java
@@ -1,34 +1,45 @@
 package ibu.edu;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class App
-{
+public class App {
     public static void main( String[] args ) throws IOException {
         String path = "C:\\Users\\Korisnik\\Desktop\\IP-COUNTRY-REGION-CITY-SHUFFLED\\IP-COUNTRY-REGION-CITY-SHUFFLED.csv";
+        String newPath = "C:\\Users\\Korisnik\\Desktop\\IP-COUNTRY-REGION-CITY-SHUFFLED\\Sorted IPs.csv";
 
         File file = new File(path);
+        File newFile = new File(newPath);
         Scanner input = new Scanner(file);
 
+        FileWriter fileWriter = new FileWriter(newFile);
+
         String[] ips;
-        IPAddress[] ipAddresses = new IPAddress[20];
+        IPAddress[] ipAddresses = new IPAddress[30];
 
         int i = 0;
-        while(input.hasNextLine() && i < 20){
+        while(input.hasNextLine() && i < 30){
+
             String data = input.nextLine();
             data = data.replace("\"", "");
             ips = data.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
             long ipFrom = Long.parseLong(ips[0]);
             long ipTo = Long.parseLong(ips[1]);
             ipAddresses[i] = new IPAddress(ipFrom, ipTo, ips[2], ips[3], ips[4], ips[5]);
-            System.out.println(ipAddresses[i]);
             i++;
 
         }
         input.close();
 
         BubbleSort.sort(ipAddresses);
+
+        for(int a = 0; a < ipAddresses.length; a++){
+            System.out.println(ipAddresses[a].ipFrom);
+            fileWriter.write("\"" + ipAddresses[a].ipFrom + "\"," + "\"" + ipAddresses[a].ipTo + "\"," + "\"" + ipAddresses[a].countryCode + "\"," + "\"" + ipAddresses[a].countryName + "\"," + "\"" + ipAddresses[a].regionName + "\"," + "\"" + ipAddresses[a].cityName + "\n");
+        }
+        fileWriter.close();
     }
 }
