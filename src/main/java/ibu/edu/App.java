0536565a53d62a61a08dd3cbdf608a04de1dec2c
MakAlijevic <alijevic.mak@gmail.com>
2022-03-28 14:48:19 +0200

    Created input from file for searching algorithm. Added to new array of sorted IPs. Created IP octet extraction method.

--- src/main/java/ibu/edu/App.java
+++ src/main/java/ibu/edu/App.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
 public class App {
     public static void main( String[] args ) throws IOException {
+
+        //Sorting algorithm
         String path = "C:\\Users\\Korisnik\\Desktop\\IP-COUNTRY-REGION-CITY-SHUFFLED\\IP-COUNTRY-REGION-CITY-SHUFFLED.csv";
         String newPath = "C:\\Users\\Korisnik\\Desktop\\IP-COUNTRY-REGION-CITY-SHUFFLED\\Sorted IPs.csv";
 
@@ -37,9 +39,51 @@ public class App {
         BubbleSort.sort(ipAddresses);
 
         for(int a = 0; a < ipAddresses.length; a++){
-            System.out.println(ipAddresses[a].ipFrom);
             fileWriter.write("\"" + ipAddresses[a].ipFrom + "\"," + "\"" + ipAddresses[a].ipTo + "\"," + "\"" + ipAddresses[a].countryCode + "\"," + "\"" + ipAddresses[a].countryName + "\"," + "\"" + ipAddresses[a].regionName + "\"," + "\"" + ipAddresses[a].cityName + "\n");
         }
         fileWriter.close();
+
+
+
+        //Searching algorithm
+        String sortedPath = "C:\\Users\\Korisnik\\Desktop\\IP-COUNTRY-REGION-CITY-SHUFFLED\\Sorted IPs.csv";
+        File sortedFile = new File(sortedPath);
+        Scanner sortedInput = new Scanner(sortedFile);
+        Scanner scanner = new Scanner(System.in);
+
+        String[] sortedIps;
+        IPAddress[] sortedIpAddresses = new IPAddress[30];
+
+        int j = 0;
+        while(sortedInput.hasNextLine() && j < 30){
+            String sortedData = sortedInput.nextLine();
+            sortedData = sortedData.replace("\"","");
+            sortedIps = sortedData.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+            long sortedIpFrom = Long.parseLong(sortedIps[0]);
+            long sortedIpTo = Long.parseLong(sortedIps[1]);
+            sortedIpAddresses[j] = new IPAddress(sortedIpFrom, sortedIpTo, sortedIps[2], sortedIps[3], sortedIps[4], sortedIps[5]);
+            j++;
+        }
+        System.out.println("Enter IP address to search for location : ");
+        String ipadd = scanner.nextLine();
+
+        //IP octet extraction method
+        int octet[] = new int[4];
+        StringBuilder tmp = new StringBuilder();
+        int counter = 0;
+        for(int p = 0; p<ipadd.length();p++){
+            if (ipadd.charAt(p) != '.') {
+                tmp.append(ipadd.charAt(p));
+            }
+            if (ipadd.charAt(p) == '.' || p == ipadd.length() - 1) {
+                octet[counter++] = Integer.parseInt(tmp.toString());
+                tmp = new StringBuilder();
+            }
+        }
+        int w = octet[0];
+        int x = octet[1];
+        int y = octet[2];
+        int z = octet[3];
     }
 }
